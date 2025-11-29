089761717702eb8124f83789369a496c81888112
Pavel Hanyš <pavhanys@gmail.com>
2025-11-29 16:26:45 +0100

    feat: dashboard update

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { formReducer } from '../features/form/formSlice';
 import { runnersReducer } from '../features/runners/runnersSlice';
 
@@ -11,3 +12,6 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
